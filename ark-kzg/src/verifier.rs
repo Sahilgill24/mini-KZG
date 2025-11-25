5922916f9303c39fc5fb645d9ba4cb2eb57ad2d1
Sahilgill24 <gursahilgill24@gmail.com>
2025-11-25 06:47:37 +0530

    refactor: refactoring the verifier

--- ark-kzg/src/verifier.rs
+++ ark-kzg/src/verifier.rs
@@ -1,40 +1,40 @@
-// pairings for verifying the proof generated.
-
-use ark_bls12_381::g1::Config as cfg1;
-
-use ark_bls12_381::g2::Config as cfg;
-use ark_bls12_381::{Config, FrConfig, G1Affine, G2Affine};
+use ark_bls12_381::{Bls12_381, Fr, G1Affine, G1Projective, G2Affine};
 use ark_ec::AffineRepr;
-use ark_ec::bls12::Bls12;
-use ark_ec::pairing::{Pairing, PairingOutput};
-use ark_ec::short_weierstrass::Projective;
-use ark_ff::{Fp, MontBackend, PrimeField};
+use ark_ec::pairing::Pairing;
+use ark_ff::PrimeField;
 
-/// e( C- [y]1, H) = e ( pi, [s-z]2)
-/// c: commitment
-/// [y]1 evaluation at z
-/// H: Generator for G2 group
-/// pi: proof
-/// [s-z]2: s2 - z2 (z implemented at G2Affine group)
+use crate::setup::SRS;
+
+/// Verifies that P(z) = y for a committed polynomial P using the pairing equation:
+/// e(C - [y]₁, H) = e(pi, [s]₂ - [z]₂)
+/// c= [p(s)]1 or the commitment
+/// pi = [q(s)]1 or the proof
+/// H = G2
+/// [y]1 = y.G1
+/// [s]2 = second element of the SRS2
+/// [z2] = z.G2
 pub fn verify_proof(
-    evaluation_points: (
-        Fp<MontBackend<FrConfig, 4>, 4>,
-        Fp<MontBackend<FrConfig, 4>, 4>,
-    ),
-    commitment: Projective<cfg1>,
-    proof: Projective<ark_bls12_381::g1::Config>,
-    powers_of_s: (Vec<Projective<cfg1>>, Vec<Projective<cfg>>),
+    z: &Fr,
+    y: &Fr,
+    commitment: &G1Projective,
+    proof: &G1Projective,
+    srs: &SRS,
 ) -> bool {
-    let y1 = evaluation_points.1;
-    let z = evaluation_points.0;
     let g1 = G1Affine::generator();
-    let h = G2Affine::generator();
-    let y1_group = g1.mul_bigint(y1.into_bigint());
-    let z2_group = h.mul_bigint(z.into_bigint());
-    let s2 = powers_of_s.1[1];
-    let val: PairingOutput<Bls12<Config>> =
-        Bls12::pairing(commitment - y1_group, G2Affine::generator());
+    let g2 = G2Affine::generator();
 
-    let val2: PairingOutput<Bls12<Config>> = Bls12::pairing(proof, s2 - z2_group);
-    val == val2
+    // [y]₁ = y · G1
+    let y_g1 = g1.mul_bigint(y.into_bigint());
+
+    //  [z]₂ = z · G2
+    let z_g2 = g2.mul_bigint(z.into_bigint());
+    let s_g2 = srs.1[1];
+
+    // Compute the left side of the pairing equation: e(C - [y]₁, H)
+    let lhs = Bls12_381::pairing(commitment - y_g1, g2);
+    println!("{:?}", lhs);
+    // Compute the right side of the pairing equation: e(π, [s]₂ - [z]₂)
+    let rhs = Bls12_381::pairing(proof, s_g2 - z_g2);
+    println!("{:?}", rhs);
+    lhs == rhs
 }
